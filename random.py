a2b54ab3aa9d4c9b191b002b9324a26dc46ef5c6
aqa1982 <37027856+aqa1982@users.noreply.github.com>
2023-10-21 16:30:17 +0300

    Add files via upload

---
+++ random.py
@@ -0,0 +1,291 @@
+import os
+
+os.system('clear')
+
+os.system("pip install requests")
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+
+def Zeek():
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system('clear')
+
+
+print("""
+\033[1;36m  ██████  ███████  █████   ███    ███  █████
+\033[1;33m ██    ██ ██      ██   ██  ██ ████ ██ ██   ██
+\033[1;33m ██    ██ ███████ ███████  ██ ████ ██ ███████
+\033[1;36m ██    ██      ██ ██   ██  ██  ██  ██ ██   ██
+\033[1;36m   ████   ███████ ██   ██  ██      ██ ██   ██                                                                                                                                      
+
+[1;31m   ██████  ███████  █████   ███    ███  █████
+          [1;32m  ██    ██ ██      ██   ██  ██ ████ ██ ██   ██
+         [1;33m  ██    ██ ███████ ███████  ██ ████ ██ ███████
+          [1;34m██    ██      ██ ██   ██  ██  ██  ██ ██   ██
+         [1;35m████   ███████ ██   ██  ██      ██ ██   ██ 
+
+
+
+ \033[1;93m×××××××××××××××××\033[1;93m××××××××××××××\033[1;93m××××××××××××××××××
+ \033[1;93m|     \033[1;96m[✓] CREATED BY\33[0;m   :  \033[1;96mOSᗩᗰᗩ-ZᗩᗰZᗩᗰ   \033[1;93m|
+ \033[1;93m|     \033[1;32m[✓] FACEBOK      : \033[1;34m AQA                \033[1;93m|
+ \033[1;93m|     \033[1;35m[✓] GITHUB       :  \033[1;35mLIBYA-1982               \033[1;93m|
+ \033[1;93m|     \033[1;36m[✓] TOOL STATUS  : \033[1;36m All To All            \033[1;93m|
+ \033[1;93m|     \033[1;35m[✓] TEAM         :  \033[1;35mKST                   \033[1;93m|
+ \033[1;93m|     \033[1;36m[✓] TOOL VIRSION :  \033[1;36m1.9                   \033[1;93m|
+ \033[1;93m×××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m×××××××××××××××××
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m PLZ SAPPORT ME BRO....
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m OSᗩᗰᗩ TERMUX HELPING ZONE....
+ \033[1;93m××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m××××××××××××××××××""")
+
+print("""
+  \033[1;91m[\033[1;97m1\033[1;91m]\033[1;32m RANDOM ID CLONE \033[1;93m{FAST}
+  \033[1;91m[\033[1;97m2\033[1;91m]\033[1;32m BANGLADESH SIM NUMBAR CLONE \033[1;91m{FAST}
+  \033[1;91m[\033[1;97m3\033[1;91m]\033[1;32m AUTO FB SHARE \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m4\033[1;91m]\033[1;32m AUTO SMS BOMBING \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m5\033[1;91m]\033[1;32m UNLIMITED COOKIE \033[1;93m{WOARKING}
+  \033[1;91m[\033[1;97m6\033[1;91m]\033[1;32m FB BROUTE FROCE \033[1;93m{TRY IT BRO}
+  \033[1;91m[\033[1;97m7\033[1;91m]\033[1;32m FILE CLONE \033[1;91m{FAST}
+  \033[1;91m[\033[1;97m8\033[1;91m]\033[1;32m OLD ID CLONE 2009-2012 \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m9\033[1;91m]\033[1;32m Open Script \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m10\033[1;91m]\033[1;32m Cookie Convart Token \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m11\033[1;91m]\033[1;32m Random Update Clone \033[1;94m{FAST TEST}
+  \033[1;91m[\033[1;97m12\033[1;91m]\033[1;32m Gf Hacke Id Link  \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m13\033[1;91m]\033[1;32m Unlimited Email \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m14\033[1;91m]\033[1;32m Unlimited Usar Agent \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m00\033[1;91m]\033[1;32m Exit
+  \033[1;93m×××××××××××××××××××××××××××××××××××××××××××××××××
+""")
+
+pil = input("  \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["02", "2"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["03", "3"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["04", "4"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["5"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+elif pil in ["6"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["7"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["8"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["9"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+
+
+elif pil in ["10"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["11"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["12"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+elif pil in ["13"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+elif pil in ["14"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+
+
+elif pil in ["15"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+elif pil in ["16"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    time.sleep(2)
+
+elif pil in ["17"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+
+
+elif pil in ["18"]:
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100024557781911")
