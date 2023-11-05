649524bdfe359b67c25c8f9f3ef48de4eaabda0d
aqa1982 <37027856+aqa1982@users.noreply.github.com>
2023-11-05 22:35:17 +0300

    Add files via upload

---
+++ AKASH.py
@@ -0,0 +1,283 @@
+import os
+
+os.system('clear')
+
+os.system("pip install requests")
+
+os.system("xdg-open https://facebook.com/groups/551365756758487/")
+
+
+def Zeek():
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/copy.link.erorr404")
+
+    os.system('clear')
+
+    os.system("xdg-open https://github.com/KgHasan")
+
+    os.system('clear')
+
+
+print("""
+ ██████  █  ██████  ███████  █████   ██      ██  █████
+\033[1;93  ██    ██ ██       ██   ██  ██ ████ ██ ██   ██
+\033[1;94  ██    ██ ███████ ███████  ██ ████ ██ ███████
+\033[1;95  ██    ██      ██ ██   ██  ██  ██  ██ ██   ██
+\033[1;96    ████   ███████ ██   ██  ██      ██ ██   ██  
+             ....... OSAMA-1982 ......
+ \033[1;93m×××××××××××××××××\033[1;93m××××××××××××××\033[1;93m××××××××××××××××××
+ \033[1;93m|     \033[1;96m[✓] CREATED BY   : \33[0;m:\033[1;96mOSᗩᗰᗩ Codes  \033[1;93m|
+ \033[1;93m|     \033[1;32m[✓] FACEBOK      : \033[1;34m O S ᗩ ᗰ ᗩ 𓆩T-2𓆪     \033[1;93m|
+ \033[1;93m|     \033[1;35m[✓] GITHUB       :  \033[1;35mO S ᗩ ᗰ ᗩ           \033[1;93m|
+ \033[1;93m|     \033[1;36m[✓] TOOL STATUS  : \033[1;36m All To All            \033[1;93m|
+ \033[1;93m|     \033[1;35m[✓] TEAM         :  \033[1;35mAQA                   \033[1;93m|
+ \033[1;93m|     \033[1;36m[✓] TOOL VIRSION :  \033[1;36m1.9                   \033[1;93m|
+ \033[1;93m×××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m×××××××××××××××××
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m PLZ SAPPORT ME BRO....
+ \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m OSᗩᗰᗩ TERMUX HELPING ZONE....
+ \033[1;93m××××××××××××××××\033[1;93m×××××××××××××××\033[1;93m××××××××××××××××××""")
+
+print("""
+  \033[1;91m[\033[1;97m1\033[1;91m]\033[1;32m RANDOM ID CLONE \033[1;93m{FAST}
+  \033[1;91m[\033[1;97m2\033[1;91m]\033[1;32m BANGLADESH SIM NUMBAR CLONE \033[1;91m{WORKING}
+  \033[1;91m[\033[1;97m3\033[1;91m]\033[1;32m AUTO FB SHARE \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m4\033[1;91m]\033[1;32m AUTO SMS BOMBING \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m5\033[1;91m]\033[1;32m UNLIMITED COOKIE \033[1;93m{WOARKING}
+  \033[1;91m[\033[1;97m6\033[1;91m]\033[1;32m FB BROUTE FROCE \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m7\033[1;91m]\033[1;32m FILE CLONE \033[1;91m{COMMING}
+  \033[1;91m[\033[1;97m8\033[1;91m]\033[1;32m OLD ID CLONE 2009-2012 \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m9\033[1;91m]\033[1;32m Open Script \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m10\033[1;91m]\033[1;32m Cookie Convart Token \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m11\033[1;91m]\033[1;32m Random Update Clone \033[1;94m{FAST TEST}
+  \033[1;91m[\033[1;97m12\033[1;91m]\033[1;32m Gf Hacke Id Link  \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m13\033[1;91m]\033[1;32m Unlimited Email \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m14\033[1;91m]\033[1;32m Unlimited Usar Agent \033[1;93m{WORKING}
+  \033[1;91m[\033[1;97m00\033[1;91m]\033[1;32m Exit
+  \033[1;93m×××××××××××××××××××××××××××××××××××××××××××××××××
+""")
+
+pil = input("  \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system('git clone https://github.com/HON3Y-KING/Degikya.git')
+
+    os.system('cd Degikya && python degikya.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["02", "2"]:
+
+    os.system('git clone https://github.com/KAMII911/Kamran.git')
+
+    os.system('cd Kamran && python KAMII.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["03", "3"]:
+
+    os.system('rm -rf M16 && git clone https://github.com/mafiat2/M16.git')
+
+    os.system('cd M16 && python Mafia.py')
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["04", "4"]:
+
+    os.system('git clone https://github💙.com/MR-XYZ-404/MR_XYZ.git')
+
+    os.system('cd MR_XYZ && run💙.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["5"]:
+
+    os.system('git clone https://github.com/KhushalVala/Randomclone')
+
+    os.system(' cd Randomclone && python Elisha.py')
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+elif pil in ["6"]:
+
+    os.system('git clone https://github.com/KgHasan/TARGET_HAKING')
+
+    os.system('cd TARGET_HAKING && python Fb_Target.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["7"]:
+
+    os.system('git clone https://github.com/SABBIR163-XD/FILEX')
+
+    os.system('cd FILEX && Python run.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["8"]:
+
+    os.system('git clone https://github.com/Mudassarch1122/Mudassar')
+
+    os.system('cd Mudassar && python Mudassar.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["9"]:
+
+    os.system("xdg-open https://github.com/KgHasan/OPEN_SCOURE-1")
+
+    time.sleep(2)
+
+
+
+elif pil in ["10"]:
+
+    os.system('git clone https://github.com/ShahWahid0785/Random-clone.git')
+
+    os.system('cd Random-clone && python WKING.pyc')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+
+
+elif pil in ["11"]:
+
+    os.system('rm -rf SIALXD && git clone https://github.com/M4STERMIND1/SIALXD.git')
+
+    os.system('cd SIALXD && python SIALZADA.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["12"]:
+
+    os.system('rm -rf MR-OSAMA && git clone https://github.com/aqa1982/MR-OSAMA.git')
+
+    os.system('cd MR-OSAMA && python BAPJI.py')
+
+    os.system("xdg-open https://facebook.com/groups/551365756758487/")
+
+elif pil in ["13"]:
+
+    os.system('rm -rf Hasan_Tempmail && git clone https://github.com/KgHasan/Hasan_Tempmail')
+
+    os.system('cd Hasan_Tempmail && python Unlimited_Email.py')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+    Zeek()
+
+elif pil in ["14"]:
+
+    os.system('git clne https://github.com/hasanraj720/RAJ.git')
+
+    os.system('cd texs && python2 xm.py')
+
+    time.sleep(2)
+
+
+
+elif pil in ["15"]:
+
+    os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+    os.system('cd texs && python yahoo.py')
+
+elif pil in ["16"]:
+
+    os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+    os.system('cd texs && python2 santo.py')
+
+    time.sleep(2)
+
+elif pil in ["17"]:
+
+    os.system('rm -rf Old-Crack && git clone https://github.com/syedzada1100/Old-Crack.git')
+
+    os.system('cd Old-Crack && python2 Old.py')
+
+
+
+elif pil in ["18"]:
+
+    os.system('git clone https://github.com/Fucker-MsM/paidfree4')
+
+    os.system('cd paidfree4 && python 20091st.py')
